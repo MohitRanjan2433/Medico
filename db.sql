65de7d20b90e4dc86172bb8283be87efc5222f97
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2023-05-16 18:43:10 +0530

    feat: added login system

---
+++ db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE medico;
+GRANT ALL ON medico.* TO 'arjun'@'localhost' IDENTIFIED by 'zappeysfc';
+GRANT ALL ON medico.* TO 'arjun'@'127.0.0.1' IDENTIFIED by 'zappeysfc';
+
+USE medico;
+CREATE TABLE users (
+	user_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(128),
+    username VARCHAR(128),
+    password VARCHAR(128),
+    INDEX(username)
+) ENGINE = InnoDB;
+
+INSERT INTO users(email, username, password) VALUES ("arjunbasandrai2004@gmail.com","Arjun122","956152c4943615f5b45ba32bb8ba4ebf");
+INSERT INTO users(email, username, password) VALUES ("text@sample.com", "Test", "6c30734811916b0f0f24a4630b08036f");
